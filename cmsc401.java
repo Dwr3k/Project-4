2fc2f42b07994e5939e501eefe6f6d598bf4b0a3
Dwr3k <mcgloneda@vcu.edu>
2019-12-04 18:11:57 -0500

    Finished 114

--- cmsc401.java
+++ cmsc401.java
@@ -1,3 +1,4 @@
+//Derrick McGlone
 import java.util.Scanner;
 
 public class cmsc401 
@@ -6,9 +7,8 @@ public class cmsc401
 	int minCost = 0;
 	int rodSize = 0;
 	int numCuts = 0;
-	boolean doneWithLeft = false;
 
-	public void inputTime()
+	public void inputTime()//Takes the input and builds the array
 	{
 		Scanner input = new Scanner(System.in);
 
@@ -22,20 +22,22 @@ public class cmsc401
 			cutSpots[input.nextInt()] = 1;
 		}
 
-		cutTime(0, rodSize, 0);
+		input.close();
+
+		cutTime(0, rodSize, 0); //Initial call to cut with full bar size being passsed
 	}
 
-	public int cutTime(int start, int end, int cutsMade)
+	public void cutTime(int start, int end, int cutsMade)
 	{
-		int segmentSize = end - start;
-		int middle = (end + start) / 2;
-		int cuttingPosition = 0;
+		int segmentSize = end - start; //Gets segment size
+		int middle = (end + start) / 2; //Finds the middle point of the segment (in terms of array position)
+		int cuttingPosition = 0; //Where the cut will happen 
 
-		if(cutSpots[middle] == 1)
+		if(cutSpots[middle] == 1)//Is there a cut spot in the middle?
 		{
 			cuttingPosition = middle;
 		}
-		else
+		else//If not, find the closest cut spot to middle
 		{
 			int minDistance = 1000;
 
@@ -52,34 +54,30 @@ public class cmsc401
 			}
 		}
 
-		minCost += segmentSize;
-		++numCuts;
-		cutSpots[cuttingPosition] = 2;
+		minCost += segmentSize; 		//Adds cost of cutting segment
+		++cutsMade;						//Increases number of cuts made
+		cutSpots[cuttingPosition] = 2;	//Makes a cut at the determined cutting
 
 		if(cutsMade < numCuts)
 		{
-			for(int i = start; i < middle; ++i)
+			for(int i = start; i < cuttingPosition; ++i)//Recursively cuts the left side of new bar until no more cut spots
 			{
 				if(cutSpots[i] == 1)
 				{
-					cutTime(start, middle, cutsMade);
-					doneWithLeft = true;
+					cutTime(start, cuttingPosition, cutsMade);
+					i = 0;
 				}
 			}
-			
-			if(doneWithLeft == true)
+
+			for(int i = cuttingPosition; i < end; ++i)//Cuts right side recursively until no more cut spots on right
 			{
-				for(int i = middle; i < end; ++i)
+				if(cutSpots[i] == 1)//If cut spot found, cut bar again
 				{
-					if(cutSpots[i] == 1)
-					{
-						cutTime(middle, end, cutsMade);
-					}
+					cutTime(cuttingPosition, end, cutsMade);
+					i = 0;
 				}
 			}
 		}
-
-		return minCost;
 	}
 
 	public static void main(String[] args)
@@ -89,4 +87,3 @@ public class cmsc401
 		System.out.println(program.minCost);
 	}
 }
-
