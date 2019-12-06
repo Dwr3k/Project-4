334c2446d947b842963e246e6286c004f78f5c62
Dwr3k <mcgloneda@vcu.edu>
2019-12-05 19:52:32 -0500

    Is this considered CHeating

--- cmsc401.java
+++ cmsc401.java
@@ -7,11 +7,9 @@ public class cmsc401
 	int minCost = 0;
 	int rodSize = 0;
 	int numCuts = 0;
-	
-	int[] secondArray;
+
+	int[] secondCuts;
 	int secondMin = 0;
-
-
 
 	public void inputTime()//Takes the input and builds the array
 	{
@@ -21,21 +19,36 @@ public class cmsc401
 		numCuts = input.nextInt();
 
 		cutSpots = new int[rodSize + 1];
-
+		secondCuts = new int[rodSize + 1];
 
 		for(int i = 0; i < numCuts; ++i)
 		{
-			cutSpots[input.nextInt()] = 1;
+			cutSpots[input.nextInt()] = 1;	
 		}
-		
-		secondArray = cutSpots;
+
+		for(int i = 0; i < cutSpots.length; ++i)
+		{
+			secondCuts[i] = cutSpots[i];
+		}
 
 		input.close();
 
 		reduxTime(0, rodSize, 0);
-		
-		System.out.println(minCost);
-		System.out.println(secondMin);
+		cutTime(0, rodSize, 0);
+
+
+		if(minCost < secondMin)
+		{
+			System.out.println(minCost);
+		}
+		else if(minCost == secondMin)
+		{
+			System.out.println(minCost);
+		}
+		else if(secondMin < minCost)
+		{
+			System.out.println(secondMin);
+		}
 	}
 
 	public void reduxTime(int start, int end, int cutsMade)
@@ -93,37 +106,37 @@ public class cmsc401
 			}
 		}
 
-		 if(clusterCut == false)
-		 {
-			 int maxCut = -100000;
-			 
-			 for(int i = 0; i < bestCuts.length; ++i)
-			 {
-				 if(bestCuts[i] > maxCut)
-				 {
-					 maxCut = bestCuts[i];
-				 }
-			 }
-			 
-			 for(int i = 0; i < bestCuts.length; ++i)
-			 {
-				 if(bestCuts[i] != maxCut)
-				 {
-					 bestCuts[i] = 0;
-				 }
-			 }
-			 
-			 int distanceFromMid = 1000000;
-			 for(int i = 0; i < bestCuts.length; ++i)
-			 {
-				 if(bestCuts[i] != 0 && Math.abs(i - middle) < distanceFromMid)
-				 {
-					 //System.out.println("Position: " + i + " has cut " + bestCuts[i]);
-					 cuttingPosition = i;
-					 distanceFromMid = Math.abs(i - middle);
-				 }
-			 }
-		 }
+		if(clusterCut == false)
+		{
+			int maxCut = -100000;
+
+			for(int i = 0; i < bestCuts.length; ++i)
+			{
+				if(bestCuts[i] > maxCut)
+				{
+					maxCut = bestCuts[i];
+				}
+			}
+
+			for(int i = 0; i < bestCuts.length; ++i)
+			{
+				if(bestCuts[i] != maxCut)
+				{
+					bestCuts[i] = 0;
+				}
+			}
+
+			int distanceFromMid = 1000000;
+			for(int i = 0; i < bestCuts.length; ++i)
+			{
+				if(bestCuts[i] != 0 && Math.abs(i - middle) < distanceFromMid)
+				{
+					//System.out.println("Position: " + i + " has cut " + bestCuts[i]);
+					cuttingPosition = i;
+					distanceFromMid = Math.abs(i - middle);
+				}
+			}
+		}
 
 		minCost += segmentSize;
 		cutSpots[cuttingPosition] = 2;
@@ -151,6 +164,59 @@ public class cmsc401
 		}
 	}
 
+	public void cutTime(int start, int end, int cutsMade)
+	{
+		int segmentSize = end - start; //Gets segment size
+		int middle = (end + start) / 2; //Finds the middle point of the segment (in terms of array position)
+		int cuttingPosition = 0; //Where the cut will happen 
+
+		if(secondCuts[middle] == 1)//Is there a cut spot in the middle?
+		{
+			cuttingPosition = middle;
+		}
+		else//If not, find the closest cut spot to middle
+		{
+			int minDistance = 1000;
+
+			for(int i = start; i < end; ++i)
+			{
+				if(secondCuts[i] == 1)
+				{
+					if(Math.abs(i - middle) < minDistance)
+					{
+						minDistance = Math.abs(i - middle);
+						cuttingPosition = i;
+					}
+				}
+			}
+		}
+
+		secondMin += segmentSize; 		//Adds cost of cutting segment
+		++cutsMade;						//Increases number of cuts made
+		secondCuts[cuttingPosition] = 2;	//Makes a cut at the determined cutting
+
+		if(cutsMade < numCuts)
+		{
+			for(int i = start; i < cuttingPosition; ++i)//Recursively cuts the left side of new bar until no more cut spots
+			{
+				if(secondCuts[i] == 1)
+				{
+					cutTime(start, cuttingPosition, cutsMade);
+					i = 0;
+				}
+			}
+
+			for(int i = cuttingPosition; i < end; ++i)//Cuts right side recursively until no more cut spots on right
+			{
+				if(secondCuts[i] == 1)//If cut spot found, cut bar again
+				{
+					cutTime(cuttingPosition, end, cutsMade);
+					i = 0;
+				}
+			}
+		}
+	}
+
 	public boolean clusterTime(int clusterFound, int start, int end)
 	{
 		int clusterBefore = cutSpots[clusterFound - 1];
@@ -173,7 +239,7 @@ public class cmsc401
 			return false;
 		}
 	}
-	
+
 	public static void main(String[] args)
 	{
 		cmsc401 program = new cmsc401();
